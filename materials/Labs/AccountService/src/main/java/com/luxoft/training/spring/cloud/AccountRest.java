e16b4a5c726559cac5d569305b8e94a2bb5fd13b
aVolovod <java2eemelkior@gmail.com>
2017-10-19 14:50:47 +0300

    lab1

--- materials/Labs/AccountService/src/main/java/com/luxoft/training/spring/cloud/AccountRest.java
+++ materials/Labs/AccountService/src/main/java/com/luxoft/training/spring/cloud/AccountRest.java
@@ -1,4 +1,33 @@
 package com.luxoft.training.spring.cloud;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.math.BigDecimal;
+
+@RestController
 public class AccountRest {
+    AccountDAO accountDAO;
+    AccountRepository accountRepository;
+
+    @Autowired
+    public AccountRest(AccountDAO accountDAO, AccountRepository accountRepository) {
+        this.accountDAO = accountDAO;
+        this.accountRepository = accountRepository;
+    }
+
+    @RequestMapping("/create")
+    public void create(@RequestParam Integer id){
+        accountDAO.create(id);
+    }
+
+    @RequestMapping("/fund/{id}")
+    public boolean fund(@PathVariable Integer id, @RequestParam BigDecimal sum){
+        return accountDAO.addBalance(id, sum.abs());
+    }
+
+//     checkout
 }
--- materials/Labs/ClientService/src/main/java/com/luxoft/training/spring/cloud/ClientRest.java
+++ materials/Labs/ClientService/src/main/java/com/luxoft/training/spring/cloud/ClientRest.java
@@ -1,4 +1,48 @@
 package com.luxoft.training.spring.cloud;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
 public class ClientRest {
+    ClientRepository clientRepository;
+    ClientDAO clientDao;
+
+    @Autowired
+    public ClientRest(ClientRepository clientRepository, ClientDAO clientDao) {
+        this.clientRepository = clientRepository;
+        this.clientDao = clientDao;
+    }
+
+    @RequestMapping("/create")
+    public ClientEntity create(@RequestParam String name) {
+        return clientDao.create(name);
+    }
+
+    @RequestMapping("/update/{id}")
+    @ResponseBody
+    public HttpStatus update(@PathVariable Integer id, @RequestParam String name) {
+        if (clientDao.update(id, name)) {
+            return HttpStatus.OK;
+        }
+        return HttpStatus.NOT_FOUND;
+    }
+
+    @RequestMapping("/delete/{id}")
+    public void delete(@PathVariable Integer id){
+        clientRepository.delete(id);
+    }
+
+    @RequestMapping("/get")
+    public List<ClientEntity> get(){
+        return clientRepository.findAll();
+    }
+
+    @RequestMapping("/get/{id}")
+    public ClientEntity get(@PathVariable Integer id){
+        return clientRepository.findOne(id);
+    }
 }
