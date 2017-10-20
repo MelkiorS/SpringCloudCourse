36936b0e86c22b7e59b8ac3264728a274e7190c6
melkiors <java2eemelkior@gmail.com>
2017-10-20 17:23:09 +0300

    lab4

--- materials/Labs/ConfigServer/src/main/java/com/luxoft/training/spring/cloud/ConfigServer.java
+++ materials/Labs/ConfigServer/src/main/java/com/luxoft/training/spring/cloud/ConfigServer.java
@@ -1,4 +1,11 @@
 package com.luxoft.training.spring.cloud;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class ConfigServer {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServer.class);
+    }
 }
